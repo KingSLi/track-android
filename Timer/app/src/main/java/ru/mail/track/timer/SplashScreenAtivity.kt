043a1fcf58a5d1464f58b28a98b41333699b0b11
maksim <polyanskimax@gmail.com>
2019-03-23 05:45:28 +0300

    splash screen modified to CountDownTimer

--- Timer/app/src/main/java/ru/mail/track/timer/SplashScreenAtivity.kt
+++ Timer/app/src/main/java/ru/mail/track/timer/SplashScreenAtivity.kt
@@ -3,25 +3,53 @@ package ru.mail.track.timer
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.os.CountDownTimer
 
 class SplashScreenAtivity : AppCompatActivity() {
+    var currTime = 0L
+    val MAX_TIME = 2L
+    val mlsec = 1000L
+    lateinit var timer : CountDownTimer
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen_ativity)
 
-        scheduleSplashScreen()
+        currTime = savedInstanceState?.get("currTime") as Long? ?: 0L
     }
-    private fun scheduleSplashScreen() {
-        val splashScreenDuration = 2000L
-        Handler().postDelayed(
-            {
-                val myIntent = Intent(this, MainActivity::class.java)
+
+    override fun onSaveInstanceState(outState: Bundle?) {
+        super.onSaveInstanceState(outState)
+        outState?.putLong("currTime", currTime)
+    }
+
+    override fun onPause() {
+        super.onPause()
+        timer.cancel()
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle?) {
+        super.onRestoreInstanceState(savedInstanceState)
+        currTime = savedInstanceState?.get("currTime") as Long? ?: 0L
+    }
+
+    override fun onResume() {
+        super.onResume()
+        timerStart()
+    }
+
+    private fun timerStart() {
+        timer = object : CountDownTimer((MAX_TIME - currTime) * mlsec, mlsec) {
+
+            override fun onTick(millisUntilFinished: Long) {
+                currTime++
+            }
+
+            override fun onFinish() {
+                val myIntent = Intent(applicationContext, MainActivity::class.java)
                 finish()
                 startActivity(myIntent)
-            },
-            splashScreenDuration
-        )
+            }
+        }.start()
     }
 }
